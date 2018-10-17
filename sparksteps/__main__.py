jwplayer/sparksteps

commit fb2b1940e41e215fdb53d87af07c592907635b27

Added support for specifying EBS volumes (#11)

sparksteps/__main__.py
@@ -14,6 +14,14 @@ Prompt parameters:
   dynamic-pricing-master:       use spot pricing for the master nodes.
   dynamic-pricing-core:         use spot pricing for the core nodes.
   dynamic-pricing-task:         use spot pricing for the task nodes.
+  ebs-volume-size-core:         size of the EBS volume to attach to core nodes in GiB.
+  ebs-volume-type-core:         type of the EBS volume to attach to core nodes (supported: [standard, gp2, io1]).
+  ebs-volumes-per-core:         the number of EBS volumes to attach per core node.
+  ebs-optimized-core:           whether to use EBS optimized volumes for core nodes.
+  ebs-volume-size-task:         size of the EBS volume to attach to task nodes in GiB.
+  ebs-volume-type-task:         type of the EBS volume to attach to task nodes.
+  ebs-volumes-per-task:         the number of EBS volumes to attach per task node.
+  ebs-optimized-task:           whether to use EBS optimized volumes for task nodes.
   ec2-key:                      name of the Amazon EC2 key pair
   ec2-subnet-id:                Amazon VPC subnet id
   help (-h):                    argparse help
@@ -104,6 +112,17 @@ def create_parser():
     parser.add_argument('--dynamic-pricing-master', action='store_true')
     parser.add_argument('--dynamic-pricing-core', action='store_true')
     parser.add_argument('--dynamic-pricing-task', action='store_true')
+
+    # EBS configuration
+    parser.add_argument('--ebs-volume-size-core', type=int)
+    parser.add_argument('--ebs-volume-type-core', type=str, default='standard')
+    parser.add_argument('--ebs-volumes-per-core', type=int, default=1)
+    parser.add_argument('--ebs-optimized-core', action='store_true')
+
+    parser.add_argument('--ebs-volume-size-task', type=int)
+    parser.add_argument('--ebs-volume-type-task', type=str, default='standard')
+    parser.add_argument('--ebs-volumes-per-task', type=int, default=1)
+    parser.add_argument('--ebs-optimized-task', action='store_true')
 
     # Deprecated arguments
     parser.add_argument('--master')

sparksteps/cluster.py
@@ -81,6 +81,20 @@ def emr_config(release_label, keep_alive=False, **kw):
         if bid_price:
             instance_group_config['Market'] = 'SPOT'
             instance_group_config['BidPrice'] = bid_price
+
+        ebs_volume_size = kw.get('ebs_volume_size_{}'.format(instance_group), 0)
+        if ebs_volume_size > 0:
+            ebs_configuration = {
+                'EbsBlockDeviceConfigs': [{
+                    'VolumeSpecification': {
+                        'VolumeType': kw.get('ebs_volume_type_{}'.format(instance_group)),
+                        'SizeInGB': ebs_volume_size
+                    },
+                    'VolumesPerInstance': kw.get('ebs_volumes_per_{}'.format(instance_group), 1)
+                }],
+                'EbsOptimized': kw.get('ebs_optimized_{}'.format(instance_group), False)
+            }
+            instance_group_config['EbsConfiguration'] = ebs_configuration
         config['Instances']['InstanceGroups'].append(instance_group_config)
 
     if kw.get('name'):

tests/test_sparksteps.py
@@ -156,6 +156,77 @@ def test_emr_spot_cluster():
                       'ServiceRole': 'EMR_DefaultRole'}
 
 
+def test_emr_ebs_storage():
+    config = emr_config('emr-5.2.0',
+                        instance_type_master='m4.large',
+                        keep_alive=False,
+                        instance_type_core='c3.8xlarge',
+                        instance_type_task='c3.8xlarge',
+                        ebs_volume_size_core=100,
+                        ebs_volume_type_core='gp2',
+                        ebs_volumes_per_core=2,
+                        ebs_volume_size_task=10,
+                        ebs_volume_type_task='io1',
+                        ebs_optimized_task=True,
+                        num_core=2,
+                        num_task=4,
+                        bid_price_master='0.05',
+                        bid_price_core='0.25',
+                        bid_price_task='0.1',
+                        name="Test SparkSteps",
+                        bootstrap_script='s3://bucket/bootstrap-actions.sh')
+    assert config == {'Instances':
+                          {'InstanceGroups': [{'InstanceCount': 1,  # NOQA: E127
+                                               'InstanceRole': 'MASTER',
+                                               'InstanceType': 'm4.large',
+                                               'Market': 'SPOT',
+                                               'BidPrice': '0.05',
+                                               'Name': 'Master Node'},
+                                              {'BidPrice': '0.25',
+                                               'InstanceCount': 2,
+                                               'InstanceRole': 'CORE',
+                                               'InstanceType': 'c3.8xlarge',
+                                               'Market': 'SPOT',
+                                               'Name': 'Core Nodes',
+                                               'EbsConfiguration': {
+                                               'EbsBlockDeviceConfigs': [{
+                                                    'VolumeSpecification': {
+                                                        'VolumeType': 'gp2',
+                                                        'SizeInGB': 100
+                                                    },
+                                                    'VolumesPerInstance': 2
+                                                }],
+                                                'EbsOptimized': False
+                                               }},
+                                              {'BidPrice': '0.1',
+                                               'InstanceCount': 4,
+                                               'InstanceRole': 'TASK',
+                                               'InstanceType': 'c3.8xlarge',
+                                               'Market': 'SPOT',
+                                               'Name': 'Task Nodes',
+                                               'EbsConfiguration': {
+                                               'EbsBlockDeviceConfigs': [{
+                                                    'VolumeSpecification': {
+                                                        'VolumeType': 'io1',
+                                                        'SizeInGB': 10
+                                                    },
+                                                    'VolumesPerInstance': 1
+                                                }],
+                                                'EbsOptimized': True
+                                               }}],
+                           'KeepJobFlowAliveWhenNoSteps': False,
+                           'TerminationProtected': False
+                           },
+                      'Applications': [{'Name': 'Hadoop'}, {'Name': 'Spark'}],
+                      'BootstrapActions': [{'Name': 'bootstrap',
+                                            'ScriptBootstrapAction': {'Path': 's3://bucket/bootstrap-actions.sh'}}],
+                      'Name': 'Test SparkSteps',
+                      'JobFlowRole': 'EMR_EC2_DefaultRole',
+                      'ReleaseLabel': 'emr-5.2.0',
+                      'VisibleToAllUsers': True,
+                      'ServiceRole': 'EMR_DefaultRole'}
+
+
 @moto.mock_s3
 def test_setup_steps():
     s3 = boto3.resource('s3', region_name=AWS_REGION_NAME)

commit 7d9ab00d6773a7cd5c5eb06b2f3b433a595c27ba

Fix TypeError raised when ebs-volume-size is not set
This bug was accidentally introduced in fb2b194.

sparksteps/__main__.py
@@ -106,6 +106,7 @@ def create_parser():
     parser.add_argument('--tags', nargs='*')
     parser.add_argument('--uploads', nargs='*')
     parser.add_argument('--maximize-resource-allocation', action='store_true')
+    # TODO: wrap lines below in a for loop?
     parser.add_argument('--instance-type-master', default='m4.large')
     parser.add_argument('--instance-type-core')
     parser.add_argument('--instance-type-task')
@@ -114,12 +115,12 @@ def create_parser():
     parser.add_argument('--dynamic-pricing-task', action='store_true')
 
     # EBS configuration
-    parser.add_argument('--ebs-volume-size-core', type=int)
+    parser.add_argument('--ebs-volume-size-core', type=int, default=0)
     parser.add_argument('--ebs-volume-type-core', type=str, default='standard')
     parser.add_argument('--ebs-volumes-per-core', type=int, default=1)
     parser.add_argument('--ebs-optimized-core', action='store_true')
 
-    parser.add_argument('--ebs-volume-size-task', type=int)
+    parser.add_argument('--ebs-volume-size-task', type=int, default=0)
     parser.add_argument('--ebs-volume-type-task', type=str, default='standard')
     parser.add_argument('--ebs-volumes-per-task', type=int, default=1)
     parser.add_argument('--ebs-optimized-task', action='store_true')

sparksteps/cluster.py
@@ -83,7 +83,7 @@ def emr_config(release_label, keep_alive=False, **kw):
             instance_group_config['BidPrice'] = bid_price
 
         ebs_volume_size = kw.get('ebs_volume_size_{}'.format(instance_group), 0)
-        if ebs_volume_size > 0:
+        if ebs_volume_size:
             ebs_configuration = {
                 'EbsBlockDeviceConfigs': [{
                     'VolumeSpecification': {