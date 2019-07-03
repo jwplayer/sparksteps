e0853a701fb5813f0aa3d2052f16f97cadd74970
Rik Heijdens <rik@jwplayer.com>
2019-07-03 19:39:16 +0200

    It is a bit early on to support 3.8
    Python 3.8 hasn't been officially released yet... Let's hold off on that
    for now.

--- setup.py
+++ setup.py
@@ -47,6 +47,5 @@ setup(
         Programming Language :: Python :: 3.5
         Programming Language :: Python :: 3.6
         Programming Language :: Python :: 3.7
-        Programming Language :: Python :: 3.8
         """).strip().splitlines(),
 )
